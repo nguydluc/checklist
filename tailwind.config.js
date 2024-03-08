760a3456a4e9a6b95122e971a592853ace2f0426
Dylan Luc <nguyd.luc@gmail.com>
2024-03-08 13:17:16 -0700

    add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "brand-yellow": "#DAA95D",
+        "brand-green": "#6A643D",
+        "brand-white": "#EEEEEE",
+        "brand-gray": "#D9D9D9",
+        "brand-red": "#B42819",
+        "brand-blue": "#071A24",
+      },
+    },
   },
   plugins: [],
 };
