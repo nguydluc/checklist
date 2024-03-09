e9d7b5b20bc70477f79999287a97f8431b2a4150
Dylan Luc <nguyd.luc@gmail.com>
2024-03-09 16:49:11 -0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
-    <header className="bg-brand-yellow">
-      <h1 className="text-3xl font-bold font-heading text-center py-3 w-full h-[10vh]">
+    <header className="bg-brand-yellow w-full h-[10vh] flex items-center justify-center fixed top-0">
+      <h1 className="text-3xl font-bold font-heading text-center py-3">
         Santa's Checklist 🎅🏻
       </h1>
     </header>
