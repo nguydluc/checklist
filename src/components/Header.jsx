fc0f10db03e77d1c088edb70dfc13218cbd88ad7
Dylan Luc <nguyd.luc@gmail.com>
2024-03-09 15:16:27 -0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="bg-brand-yellow">
       <h1 className="text-3xl font-bold font-heading text-center py-3 w-full h-[10vh]">
-        Santa's Checklist
+        Santa's Checklist 🎅🏻
       </h1>
     </header>
   );
