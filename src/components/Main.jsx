4008d94d0605d5340250e558aa863e99cd2610a2
Dylan Luc <nguyd.luc@gmail.com>
2024-03-09 14:25:05 -0700

    add main

---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import PropTypes from "prop-types";
+
+const Main = ({ children }) => {
+  return <main>{children}</main>;
+};
+
+Main.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Main;
