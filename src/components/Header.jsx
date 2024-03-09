3a19b31837a81380a2e22ec1c729542ce43c5d43
Dylan Luc <nguyd.luc@gmail.com>
2024-03-09 14:24:43 -0700

    add header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header className="bg-brand-yellow">
+      <h1 className="text-3xl font-bold font-heading text-center py-3 w-full h-[10vh]">
+        Santa's Checklist
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
