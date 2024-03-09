d740c4e18bb145e7588702ba36e0207c1fc14c16
Dylan Luc <nguyd.luc@gmail.com>
2024-03-09 16:48:52 -0700

    add checklist

---
+++ src/components/CheckList.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+const CheckList = () => {
+  const [sortBy, setSortBy] = useState("input");
+
+  return (
+    <div className="mx-auto px-[10%] py-[5%] w-[70%] h-[50vh] md:h-[60vh] max-w-screen-lg fixed top-[40vh] md:top-[30vh] overflow-auto left-0 right-0">
+      <ul className="grid md:grid-cols-3 gap-3 mb-[10%]">
+        <li className="text-brand-green text-center">☑️ Item 1 ❌</li>
+        <li className="text-brand-green text-center">☑️ Item 2 ❌</li>
+        <li className="text-brand-green text-center">☑️ Item 3 ❌</li>
+        <li className="text-brand-green text-center">☑️ Item 4 ❌</li>
+        <li className="text-brand-green text-center">☑️ Item 5 ❌</li>
+        <li className="text-brand-green text-center">☑️ Item 6 ❌</li>
+        <li className="text-brand-green text-center">☑️ Item 7 ❌</li>
+        <li className="text-brand-green text-center">☑️ Item 8 ❌</li>
+      </ul>
+      <div className="flex flex-col md:flex-row justify-center items-center">
+        <select
+          className="px-5 py-3 rounded-3xl font-semibold bg-brand-yellow text-brand-white"
+          value={sortBy}
+          onChange={(e) => setSortBy(e.target.value)}
+        >
+          <option value="input">Sort by the input order</option>
+          <option value="description">Sort by description</option>
+          <option value="prepared">Sort by prepared status</option>
+        </select>
+        <button className="px-5 py-3 hover:text-brand-red duration-300">
+          Clear list
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CheckList;
